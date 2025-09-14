ee729c790e26fcb141c1a14ed40dbdf91e282499
Apekshit Sonawane <apekshit.sonawane@iksula.com>
2025-09-14 20:37:15 +0530

    Users component refactored and made functional component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
-
-export class Users extends Component {
-  
-  render() {
-    const { users } = this.props;
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
         {users.map((user) => {
@@ -20,12 +21,17 @@ export class Users extends Component {
       </div>
     );
   }
-}
+};
 
 const userStyle = {
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
-  gridGap: '1rem'
-}
+  gridGap: '1rem',
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 
 export default Users;
