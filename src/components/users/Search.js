cfed048ca5dfdfa1f8f2d16f2b6ec77c50cd5fee
Apekshit Sonawane <apekshit.sonawane@iksula.com>
2025-09-14 22:28:11 +0530

    Search component created and added in Main component

---
+++ src/components/users/Search.js
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+  state = {
+    text: '',
+  };
+
+  onChange = (e) => {
+    
+    // TODO: Note for dev
+    // for out case to update state we will use this
+    // this.setState({ text: e.target.value });
+
+    // TODO: Note for dev
+    // to make below resuable for all input field
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    // this 
+    e.preventDefault();
+    console.log(this.state.text)
+  }
+
+  render() {
+    return (
+      <div>
+        <form className="form" onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            name="text"
+            placeholder="Search Users"
+            onChange={this.onChange}
+            value={this.state.text}
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
+        </form>
+      </div>
+    );
+  }
+}
