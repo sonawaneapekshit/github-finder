e757856fc2901a87c3fa9797e47954ba01cdd2bd
Apekshit Sonawane <apekshit.sonawane@iksula.com>
2025-09-14 18:36:48 +0530

    State moved to app level component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,24 @@ import React, { Component } from 'react';
 import './App.css'
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 class App extends Component {
 
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount () {
+
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users')
+
+    this.setState({ users:res.data, loading: false });
+
+    console.log(res.data)
+  }
 
   render() {
 
@@ -11,7 +27,7 @@ class App extends Component {
       <div className="App">
        <Navbar />
        <div className="container">
-        <Users />
+        <Users loading={this.state.loading} users={this.state.users}  />
        </div>
       </div>
     );
